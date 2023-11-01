74ae0ec18ca4c8ee12d0ce2216595cb03c365921
Maicon de Menezes <maicon@kamico.com.br>
2023-11-01 15:53:59 -0300

    fix businessSegment

--- views/tb_invoice.sql
+++ views/tb_invoice.sql
@@ -1,13 +1,13 @@
 -- db_uc_kami.tb_invoice source
 CREATE OR REPLACE
 ALGORITHM = UNDEFINED VIEW `tb_invoice` AS
-SELECT DISTINCT
+SELECT DISTINCTROW
     CAST(`empresa`.`cnpj` AS CHAR charset utf8mb4) AS `distributorDocument`,
-    `nota_fiscal`.`dt_emissao`, '%d/%m/%Y' AS `invoiceDate`,
-    IFNULL(CAST(`empresa`.`cod_empresa` AS CHAR charset utf8mb4), '0') AS `salesChannelCode`,
+    `nota_fiscal`.`dt_emissao` AS `invoiceDate`,
+    IFNULL(CAST(`ramo_atividade`.`desc_abrev` AS CHAR charset utf8mb4), '0') AS `salesChannelCode`,
     IFNULL(CAST(`nota_fiscal`.`cnpj` AS CHAR charset utf8mb4), '0') AS `document`,
     IFNULL(CAST(`nota_fiscal`.`nome_cliente` AS CHAR charset utf8mb4), '0') AS `fullname`,
-    'TRUSS' AS `businessSegment`,
+    IFNULL(CAST(`ramo_atividade`.`desc_abrev` AS CHAR charset utf8mb4), '0') AS `businessSegment`,
     IFNULL(CAST(`nota_fiscal`.`sigla_uf` AS CHAR charset utf8mb4), '0') AS `state`,
     IFNULL(CAST(`nota_fiscal`.`cidade` AS CHAR charset utf8mb4), '0') AS `city`,
     IFNULL(CAST(`nota_fiscal`.`bairro` AS CHAR charset utf8mb4), '0') AS `district`,
@@ -46,38 +46,41 @@ SELECT DISTINCT
     IFNULL(CAST(REPLACE(ROUND(`nota_fiscal`.`vl_despesas`, 2), '.', ',') AS CHAR charset utf8mb4), '0') AS `otherExpensesTotal`,
     IFNULL(CAST(REPLACE(ROUND(((`nota_fiscal_item`.`preco_total` / `nota_fiscal`.`vl_total_produtos`) * `nota_fiscal`.`vl_frete`), 2), '.', ',') AS CHAR charset utf8mb4), '0') AS `freightTotal`,
     IFNULL(CAST(REPLACE(ROUND(((`nota_fiscal_item`.`preco_total` * `nota_fiscal`.`vl_desconto`) / `nota_fiscal`.`vl_total_produtos`), 2), '.', ',') AS CHAR charset utf8mb4), '0') AS `discountTotal`,
-    CONCAT(CONVERT(DATE_FORMAT(`nota_fiscal`.`dt_emissao`, '%d%m%Y') using latin1), CONCAT(`nota_fiscal`.`cod_nota_fiscal`, `nota_fiscal_item`.`cod_produto`), `nota_fiscal_item`.`nr_sequencia_nf`) AS `identifier`,
+    CONCAT(CONVERT(date_format(`nota_fiscal`.`dt_emissao`, '%d%m%Y') using latin1), CONCAT(`nota_fiscal`.`cod_nota_fiscal`, `nota_fiscal_item`.`cod_produto`), `nota_fiscal_item`.`nr_sequencia_nf`) AS `identifier`,
     (CASE
         WHEN (`nota_fiscal`.`situacao` <> 200) THEN NULL
-        ELSE CAST(DATE_FORMAT(`nota_fiscal`.`dt_retorno`, '%d/%m/%Y') AS CHAR charset utf8mb4)
+        ELSE CAST(date_format(`nota_fiscal`.`dt_retorno`, '%d/%m/%Y') AS CHAR charset utf8mb4)
     END) AS `invoiceCancelledOn`,
     (CASE
         WHEN (`nota_fiscal`.`situacao` = 200) THEN 'Cancelled'
         ELSE 'Active'
     END) AS `invoiceStatus`
-FROM (((((((((`vd_nota_fiscal` `nota_fiscal`
-JOIN `vd_nota_fiscal_item` `nota_fiscal_item` 
-    ON (((`nota_fiscal`.`cod_empresa` = `nota_fiscal_item`.`cod_empresa`)
-    AND (`nota_fiscal`.`cod_nota_fiscal` = `nota_fiscal_item`.`cod_nota_fiscal`))))
-JOIN `cd_produto` `produto`
-    ON ((`nota_fiscal_item`.`cod_produto` = `produto`.`cod_produto`)))
-JOIN `cd_cliente_endereco` `cliente_endereco`
-    ON ((`nota_fiscal`.`cod_cliente` = `cliente_endereco`.`cod_cliente`)))
-JOIN `vd_nota_fiscal_atividade` `nota_fiscal_atividade`
-    ON (((`nota_fiscal`.`cod_nota_fiscal` = `nota_fiscal_atividade`.`cod_nota_fiscal`)
-    AND (`nota_fiscal_item`.`cod_nota_fiscal` = `nota_fiscal_atividade`.`cod_nota_fiscal`))))
-JOIN `cd_ramo_atividade` `ramo_atividade` 
-    ON ((`nota_fiscal_atividade`.`cod_ramo_atividade` = `ramo_atividade`.`cod_ramo_atividade`)))
-JOIN `cd_cliente_atividade` `cliente_atividade` 
-    ON (((`nota_fiscal`.`cod_cliente` = `cliente_atividade`.`cod_cliente`)
-    AND (`nota_fiscal_atividade`.`cod_ramo_atividade` = `cliente_atividade`.`cod_ramo_atividade`)
-    AND (`ramo_atividade`.`cod_ramo_atividade` = `cliente_atividade`.`cod_ramo_atividade`))))
-JOIN `cd_empresa` `empresa`
-    ON (((`nota_fiscal`.`cod_empresa` = `empresa`.`cod_empresa`)
-    AND (`nota_fiscal_item`.`cod_empresa` = `empresa`.`cod_empresa`))))
-JOIN `sg_colaborador` `vendedor`
-    ON ((`nota_fiscal`.`cod_colaborador` = `vendedor`.`cod_colaborador`)))
-JOIN `cd_cond_pagto` `cond_pagto`
-    ON ((`nota_fiscal`.`cod_cond_pagto` = `cond_pagto`.`cod_cond_pagto`)))
-WHERE (`nota_fiscal`.`cod_empresa` IN (1, 2, 3, 6, 13))
-ORDER BY `nota_fiscal`.`dt_emissao`;
+FROM
+    (((((((((`vd_nota_fiscal` `nota_fiscal`
+JOIN `vd_nota_fiscal_item` `nota_fiscal_item` ON
+    (((`nota_fiscal`.`cod_empresa` = `nota_fiscal_item`.`cod_empresa`)
+        AND (`nota_fiscal`.`cod_nota_fiscal` = `nota_fiscal_item`.`cod_nota_fiscal`))))
+JOIN `cd_produto` `produto` ON
+    ((`nota_fiscal_item`.`cod_produto` = `produto`.`cod_produto`)))
+JOIN `cd_cliente_endereco` `cliente_endereco` ON
+    ((`nota_fiscal`.`cod_cliente` = `cliente_endereco`.`cod_cliente`)))
+JOIN `vd_nota_fiscal_atividade` `nota_fiscal_atividade` ON
+    (((`nota_fiscal`.`cod_nota_fiscal` = `nota_fiscal_atividade`.`cod_nota_fiscal`)
+        AND (`nota_fiscal_item`.`cod_nota_fiscal` = `nota_fiscal_atividade`.`cod_nota_fiscal`))))
+JOIN `cd_ramo_atividade` `ramo_atividade` ON
+    ((`nota_fiscal_atividade`.`cod_ramo_atividade` = `ramo_atividade`.`cod_ramo_atividade`)))
+JOIN `cd_cliente_atividade` `cliente_atividade` ON
+    (((`nota_fiscal`.`cod_cliente` = `cliente_atividade`.`cod_cliente`)
+        AND (`nota_fiscal_atividade`.`cod_ramo_atividade` = `cliente_atividade`.`cod_ramo_atividade`)
+            AND (`ramo_atividade`.`cod_ramo_atividade` = `cliente_atividade`.`cod_ramo_atividade`))))
+JOIN `cd_empresa` `empresa` ON
+    (((`nota_fiscal`.`cod_empresa` = `empresa`.`cod_empresa`)
+        AND (`nota_fiscal_item`.`cod_empresa` = `empresa`.`cod_empresa`))))
+JOIN `sg_colaborador` `vendedor` ON
+    ((`nota_fiscal`.`cod_colaborador` = `vendedor`.`cod_colaborador`)))
+JOIN `cd_cond_pagto` `cond_pagto` ON
+    ((`nota_fiscal`.`cod_cond_pagto` = `cond_pagto`.`cod_cond_pagto`)))
+WHERE
+    (`nota_fiscal`.`cod_empresa` IN (1, 2, 3, 6, 13))
+ORDER BY
+    `nota_fiscal`.`dt_emissao`;
